542ab08f770be73a93b6f79a3c878728cce71634
Carson <21carson.wright@boiseschools.net>
2021-05-05 23:30:19 -0600

    Changed Colors Again

--- src/gatsby-plugin-theme-ui/index.js
+++ src/gatsby-plugin-theme-ui/index.js
@@ -13,13 +13,13 @@ const theme = merge(tailwind, {
     divide: tailwind.colors.gray[4],
     modes: {
       dark: {
-        text: tailwind.colors.gray[1],
-        primary: tailwind.colors.gray[8],
+        text: tailwind.colors.gray[4],
+        primary: tailwind.colors.gray[4],
         secondary: `#7f8ea3`,
         toggleIcon: tailwind.colors.gray[4],
         background: `#111216`,
         heading: tailwind.colors.white,
-        divide: tailwind.colors.gray[1],
+        divide: tailwind.colors.gray[8],
         muted: tailwind.colors.gray[8],
       },
     },
